8d3c5cb1c61f4577ab6edea84f928ab9ea82843d
Albert Weinert <info@der-albert.com>
2013-04-02 14:15:17 +0200

    Demo 3: Filter Date

--- AngularJSApril2013/app/app.js
+++ AngularJSApril2013/app/app.js
@@ -3,8 +3,8 @@
 function Demo2Ctrl ($scope)
 {
     $scope.items = [
-        {name: 'Früh', price: 1.20},
-        {name: 'Reisdorf', price: 1.30},
-        {name: 'Gaffel', price: 1.25}
+        {name: 'Früh', price: 1.20, lastDrunk: '2013-03-02'},
+        {name: 'Reisdorf', price: 1.30, lastDrunk: '2013-02-01'},
+        {name: 'Gaffel', price: 1.25, lastDrunk: '2012-12-01'}
     ];
 }
