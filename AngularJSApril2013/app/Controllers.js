ac1fac5b9bf7ca224e4fe44d878237024e079bfd
Albert Weinert <info@der-albert.com>
2013-04-02 16:04:26 +0200

    Demo3: With a litte bit of structure

---
+++ AngularJSApril2013/app/Controllers.js
@@ -0,0 +1,34 @@
+﻿var April;
+
+(function(april) {
+
+    (function (controllers) {
+        
+        function demo3Ctrl($scope) {
+            $scope.items = [
+                { name: 'Früh', price: 1.20, lastDrunk: '2013-03-02' },
+                { name: 'Reisdorf', price: 1.30, lastDrunk: '2013-02-01' },
+                { name: 'Gaffel', price: 1.25, lastDrunk: '2012-12-01' }
+            ];
+
+            $scope.addNewItem = function () {
+                $scope.items.push({ name: $scope.name, price: $scope.price, lastDrunk: new Date() });
+                $scope.name = '';
+                $scope.price = '';
+            };
+
+            $scope.removeItem = function (item) {
+                $scope.items.splice($scope.items.indexOf(item), 1);
+            };
+
+            $scope.beerCountText = {
+                0: 'Kein Bier!',
+                1: 'Nur noch ein Bier vorhanden',
+                other: 'Es sind {} Bier vorhanden'
+            };
+        }
+
+        controllers.Demo3  = demo3Ctrl;
+
+    })(april.Controllers || (april.Controllers = {}));
+})(April || (April = {}));
--- AngularJSApril2013/app/app.js
+++ AngularJSApril2013/app/app.js
@@ -1,27 +1,6 @@
 ﻿
+(function(angular, ns) {
+    var module = angular.module('april', []);
+    module.controller("Demo3", ns.Controllers.Demo3);
+})(angular, April);
 
-function Demo2Ctrl ($scope)
-{
-    $scope.items = [
-        {name: 'Früh', price: 1.20, lastDrunk: '2013-03-02'},
-        {name: 'Reisdorf', price: 1.30, lastDrunk: '2013-02-01'},
-        {name: 'Gaffel', price: 1.25, lastDrunk: '2012-12-01'}
-    ];
-
-    $scope.addNewItem = function(){
-        $scope.items.push({name:$scope.name, price : $scope.price, lastDrunk : new Date()});
-        $scope.name='';
-        $scope.price='';
-    }
-
-    $scope.removeItem = function(item) {
-        $scope.items.splice($scope.items.indexOf(item),1)
-    }
-
-    $scope.beerCountText ={
-          0 : 'Kein Bier!',
-          1 : 'Nur noch ein Bier vorhanden',
-          other : 'Es sind {} Bier vorhanden'
-    }
-
-}
