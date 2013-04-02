5492c2aa5e69a51280f701ac3fef813fc618e232
Albert Weinert <info@der-albert.com>
2013-04-02 14:03:49 +0200

    Demo 2: add item

--- AngularJSApril2013/app/app.js
+++ AngularJSApril2013/app/app.js
@@ -3,4 +3,9 @@
 function Demo2Ctrl ($scope)
 {
     $scope.items = [ { name: 'Köln'},{name : 'Bonn'}, {name:'Berlin'}];
+
+    $scope.addNewItem = function() {
+        $scope.items.push({ name: $scope.name });
+        $scope.name='';
+    }
 }
